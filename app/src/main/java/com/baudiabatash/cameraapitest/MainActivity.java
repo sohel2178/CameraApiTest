293bf19301f425688af28d7a7c1d578d8b61b0c3
Sohel <sohel.ahmed2178@gmail.com>
2017-08-02 15:45:57 +0600

    2nd Commit

--- app/src/main/java/com/baudiabatash/cameraapitest/MainActivity.java
+++ app/src/main/java/com/baudiabatash/cameraapitest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.baudiabatash.cameraapitest;
 
 import android.Manifest;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -8,20 +9,35 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureFailure;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CaptureResult;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.IntDef;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -32,7 +48,18 @@ import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final SparseIntArray ORIENTATION = new SparseIntArray();
+    static {
+        ORIENTATION.append(Surface.ROTATION_0,90);
+        ORIENTATION.append(Surface.ROTATION_90,0);
+        ORIENTATION.append(Surface.ROTATION_180,270);
+        ORIENTATION.append(Surface.ROTATION_270,180);
+    }
     private static final int REQUIRED_PERMISSION = 2000;
+    private static final int ACTIVITY_START_CAMERA_APP = 0;
+    private static final int STATE_PREVIEW = 0;
+    private static final int STATE_WAIT_LOCK = 1;
+    private int mState;
     private TextureView textureView;
     private Button btnTakePhoto;
 
@@ -101,11 +128,93 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private CameraCaptureSession cameraCaptureSession;
     private CameraCaptureSession.CaptureCallback cameraCaptureSessionCallback =
             new CameraCaptureSession.CaptureCallback() {
+                // Create as method here to Process Capture Result
+                private void process(CaptureResult result){
+
+                    switch (mState){
+                        case STATE_PREVIEW:
+                            break;
+
+                        case STATE_WAIT_LOCK:
+                            Integer autoFocusState = result.get(CaptureResult.CONTROL_AF_STATE);
+                            if(autoFocusState==CaptureResult.CONTROL_AF_STATE_FOCUSED_LOCKED){
+                                /*unlockFocus();
+                                Toast.makeText(getApplicationContext(), "Focus Lock Successfully", Toast.LENGTH_SHORT).show();*/
+                                captureStillImage();
+                            }
+                            break;
+                    }
+
+                }
                 @Override
                 public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
                     super.onCaptureStarted(session, request, timestamp, frameNumber);
                 }
+
+                @Override
+                public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                    super.onCaptureCompleted(session, request, result);
+                    // Call Process Method
+                    process(result);
+                }
+
+                @Override
+                public void onCaptureFailed(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull CaptureFailure failure) {
+                    super.onCaptureFailed(session, request, failure);
+                    Toast.makeText(getApplicationContext(), "Focus Lock  unSuccessful", Toast.LENGTH_SHORT).show();
+                }
             };
+
+    private HandlerThread backgroundThread;
+    private Handler backgroundHandler;
+
+    // Image File
+    private static File imageFile;
+    private ImageReader imageReader;
+    // A listener is needed
+    private final ImageReader.OnImageAvailableListener imageAvailavleListener
+            = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            backgroundHandler.post(new ImageSaver(reader.acquireNextImage()));
+
+        }
+    };
+    private static class ImageSaver implements Runnable{
+        private final Image image;
+
+        private ImageSaver(Image image) {
+            this.image = image;
+        }
+
+        @Override
+        public void run() {
+
+            ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes= new byte[byteBuffer.remaining()];
+            byteBuffer.get(bytes);
+
+            // Create a File Input Stream
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(imageFile);
+                fileOutputStream.write(bytes);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                image.close();
+                if(fileOutputStream!=null){
+                    try {
+                        fileOutputStream.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+        }
+    }
 
     private void createCameraPreview() {
         try {
@@ -116,7 +225,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             previewCaptureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
             previewCaptureRequestBuilder.addTarget(previewSurface);
             //Create a Camera Sesson Here
-            cameraDevice.createCaptureSession(Arrays.asList(previewSurface),
+            cameraDevice.createCaptureSession(Arrays.asList(previewSurface,imageReader.getSurface()),
                     new CameraCaptureSession.StateCallback() {
                         @Override
                         public void onConfigured(@NonNull CameraCaptureSession session) {
@@ -128,7 +237,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 previewCaptureRequest=previewCaptureRequestBuilder.build();
                                 cameraCaptureSession=session;
                                 cameraCaptureSession.setRepeatingRequest(previewCaptureRequest,
-                                        cameraCaptureSessionCallback,null);
+                                        cameraCaptureSessionCallback,backgroundHandler);
 
                             }catch (CameraAccessException e){
                                 e.printStackTrace();
@@ -161,6 +270,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 
                 StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                //get Largest Image Size
+                Size largestImageSize = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                        new Comparator<Size>() {
+                            @Override
+                            public int compare(Size o1, Size o2) {
+                                return Long.signum(o1.getWidth()*o1.getHeight()-o2.getWidth()*o2.getHeight());
+                            }
+                        });
+
+                // Initialize Image Reader Here
+                imageReader = ImageReader.newInstance(largestImageSize.getWidth(),largestImageSize.getHeight(),
+                        ImageFormat.JPEG,1);
+                // Set the listener
+                imageReader.setOnImageAvailableListener(imageAvailavleListener,backgroundHandler);
                 //set Camera Preview
                 previewSize = getPreviewSize(map.getOutputSizes(SurfaceTexture.class),width,height);
                 //set the Camera Id
@@ -208,7 +331,119 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         try {
             Log.d("HHH","I am in openCamera");
             //noinspection MissingPermission
-            cameraManager.openCamera(cameraId,stateCallback,null);
+            cameraManager.openCamera(cameraId,stateCallback,backgroundHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if(cameraCaptureSession!=null){
+            cameraCaptureSession.close();
+            cameraCaptureSession=null;
+        }
+
+        if(cameraDevice!=null){
+            cameraDevice.close();
+            cameraDevice=null;
+        }
+
+        if(imageReader!=null){
+            imageReader.close();
+            imageReader=null;
+        }
+    }
+
+    private void openBackgroundThread(){
+        //init background Thread
+        backgroundThread = new HandlerThread("Camera2Api Thread");
+        backgroundThread.start();
+
+        //init background Handler
+        backgroundHandler = new Handler(backgroundThread.getLooper());
+    }
+
+    private void closeBackgroundThread(){
+
+        if(backgroundThread!=null){
+            backgroundThread.quitSafely();
+
+            try {
+                backgroundThread.join();
+                backgroundThread=null;
+                backgroundHandler=null;
+
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private void takePhoto(){
+        imageFile = createImageFile();
+        lockFocus();
+    }
+
+    private File createImageFile() {
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), getString(R.string.app_name));
+
+        Log.d("JJJJJ",file.getAbsolutePath());
+
+        return new File(file.getAbsolutePath(),"sohel.jpg");
+    }
+
+    private void lockFocus(){
+        try {
+            mState= STATE_WAIT_LOCK;
+            previewCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
+                    CaptureRequest.CONTROL_AF_TRIGGER_START);
+
+            cameraCaptureSession.capture(previewCaptureRequestBuilder.build(),
+                    cameraCaptureSessionCallback,backgroundHandler);
+        }catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+        // Change the state
+
+    }
+
+    private void unlockFocus(){
+        try {
+            mState= STATE_PREVIEW;
+            previewCaptureRequestBuilder.set(CaptureRequest.CONTROL_AF_TRIGGER,
+                    CaptureRequest.CONTROL_AF_TRIGGER_CANCEL);
+
+            cameraCaptureSession.capture(previewCaptureRequestBuilder.build(),
+                    cameraCaptureSessionCallback,backgroundHandler);
+        }catch (CameraAccessException e){
+            e.printStackTrace();
+        }
+        // Change the state
+
+    }
+
+    private void captureStillImage(){
+        try {
+            CaptureRequest.Builder builder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+            builder.addTarget(imageReader.getSurface());
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            builder.set(CaptureRequest.JPEG_ORIENTATION,ORIENTATION.get(rotation));
+
+            CameraCaptureSession.CaptureCallback cameraCaptureCallback =
+                    new CameraCaptureSession.CaptureCallback() {
+                        @Override
+                        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+                            super.onCaptureCompleted(session, request, result);
+                            Toast.makeText(getApplicationContext(), "Capture Completed", Toast.LENGTH_SHORT).show();
+                            unlockFocus();
+
+                        }
+                    };
+
+            cameraCaptureSession.capture(builder.build(),cameraCaptureCallback,null);
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
@@ -229,11 +464,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @AfterPermissionGranted(REQUIRED_PERMISSION)
     private void requiredPermission() {
-        String[] perms = {Manifest.permission.CAMERA};
+        String[] perms = {Manifest.permission.CAMERA,Manifest.permission.WRITE_EXTERNAL_STORAGE};
         if (EasyPermissions.hasPermissions(this, perms)) {
             // Already have permission, do the thing
             // ...
+            openBackgroundThread();
             if(textureView.isAvailable()){
+                setupCamera(textureView.getWidth(),textureView.getHeight());
+                openCamera();
 
             }else {
                 textureView.setSurfaceTextureListener(surfaceTextureListener);
@@ -245,6 +483,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     REQUIRED_PERMISSION, perms);
         }
     }
+    
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -257,8 +496,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     protected void onPause() {
+        closeCamera();
+        closeBackgroundThread();
+
         super.onPause();
     }
+
+
 
     private void initView() {
         textureView = (TextureView) findViewById(R.id.texture_view);
@@ -268,6 +512,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        takePhoto();
 
     }
 }
